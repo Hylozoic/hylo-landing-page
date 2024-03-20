6a8d022841afeb44746e29cbc3634bff71001e62
Tibet Sprague <tibet.sprague@gmail.com>
2024-03-19 20:03:17 -0700

    Try changing asset path

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  assetPrefix: `/static`,
   siteMetadata: {
     title: `Hylo: Prosocial Coordination for Purpose-Driven Groups`,
     description: `Hylo is the leading prosocial coordination and collaboration technology for purpose-driven groups.`,
