8d5982a5bf99c08a0ea14a741cfec1b1c64c3f7b
Tibet Sprague <tibet.sprague@gmail.com>
2025-06-26 14:20:59 -0700

    Remove duplicate agreement

--- src/pages/agreements.js
+++ src/pages/agreements.js
@@ -256,10 +256,6 @@ const AgreementsPage = () => {
                     <li>We do not allow public or private harassment such as threats, intimidation, pressure, or unwanted attention.</li>
                     <li>It is never ok to threaten or incite violence against any persons, groups, or 'types' of people, or glorify, or express desire for violence or harm, or to promote violent and hateful entities.</li>
                     <li>It is unacceptable to characterize anyone's personality or behaviors, or make jokes about them, on the basis of inherent traits such as national origin, ethnicity, race, gender, gender identity and expression, sexual orientation, disability or medical condition, or lifestyle factors such as parenthood or employment.</li>
-                    <li>Characterizing anyone's personality or behaviors, or making jokes about them,
-                        on the basis of inherent traits such as national origin, ethnicity, race, gender,
-                        gender identity and expression, sexual orientation, disability or medical condition, or lifestyle factors
-                        such as parenthood or employment.</li>
                     <li>Suicide: We want Hylo to remain a safe space that encourages mental health and wellbeing, so you may not promote or encourage suicide or self-harm.</li>
                 </ol>
             </div>
